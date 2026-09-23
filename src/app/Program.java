09c772b16294237da82471888ceed344b402ad9a
molnarbea <molnarbeatrix11@gmail.com>
2026-09-23 15:53:51 +0200

    switch done es jo

--- src/app/Program.java
+++ src/app/Program.java
@@ -20,16 +20,16 @@ public class Program {
                 }   break;
                 case 2:
                 for (int i = 1; i <= 7; i++) {
-                    uj[i+7] = pakli[19 - (i - 1) * 3];
-                    uj[i] = pakli[20 - (i - 1) * 3];
-                    uj[i + 14] = pakli[21 - (i - 1) * 3];
+                    pakli[i+7] = regiPakli[19 - (i - 1) * 3];
+                    pakli[i] = regiPakli[20 - (i - 1) * 3];
+                    pakli[i + 14] = regiPakli[21 - (i - 1) * 3];
                 }
                 break;
             case 3:
                 for (int i = 1; i <= 7; i++) {
-                    uj[i+7] = pakli[19 - (i - 1) * 3];
-                    uj[i + 14] = pakli[21 - (i - 1) * 3];
-                    uj[i] = pakli[20 - (i - 1) * 3];
+                    pakli[i+7] = regiPakli[19 - (i - 1) * 3];
+                    pakli[i + 14] = regiPakli[21 - (i - 1) * 3];
+                    pakli[i] = regiPakli[20 - (i - 1) * 3];
                 }
                 break;
             default:
