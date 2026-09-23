3f3d787055b3a2873d76b516ed3a22098db201a3
Frnc36 <magoriferenc19@gmail.com>
2026-09-23 15:49:42 +0200

    switch case 2,3

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,20 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package app;
 
-/**
- *
- * @author MágoriFerencFerdinán
- */
 public class Program {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        switch (var) {
+            case val:
+                
+                break;
+            case 2:
+                for (int i = 1; i <= 7; i++) {
+                    uj[i+7] = pakli[19 - (i - 1) * 3];
+                    uj[i] = pakli[20 - (i - 1) * 3];
+                    uj[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                break;
+            case 3:
+                for (int i = 1; i <= 7; i++) {
+                    uj[i+7] = pakli[19 - (i - 1) * 3];
+                    uj[i + 14] = pakli[21 - (i - 1) * 3];
+                    uj[i] = pakli[20 - (i - 1) * 3];
+                }
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
+
+    
     
 }
